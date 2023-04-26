24108778fe4d4ab3e1464202a4d0947955e50962
이상현 <lsh80165@gmail.com>
2023-04-26 22:39:26 +0900

    Feat : 프로젝트 init

---
+++ gamtori/src/main/java/vessel/gamtori/GamtoriApplication.java
@@ -0,0 +1,13 @@
+package vessel.gamtori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamtoriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamtoriApplication.class, args);
+	}
+
+}
