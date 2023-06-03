e6f998c29f6aedcf5973d83fdf6ed1225d75e7ad
이상현 <lsh80165@gmail.com>
2023-06-03 22:09:49 +0900

    Feat : 채팅 메시지 테이블 매핑 및 enum 추가

---
+++ gamtori/src/main/java/vessel/gamtori/domain/entity/ChatMessageType.java
@@ -0,0 +1,5 @@
+package vessel.gamtori.domain.entity;
+
+public enum ChatMessageType {
+	JOIN, CHAT, OUT
+}
--- gamtori/src/main/java/vessel/gamtori/domain/entity/LiveChat.java
+++ gamtori/src/main/java/vessel/gamtori/domain/entity/LiveChat.java
@@ -4,12 +4,18 @@ import static javax.persistence.GenerationType.*;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.DynamicUpdate;
+
+import com.sun.istack.NotNull;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -40,15 +46,24 @@ public class LiveChat extends BaseTimeEntity {
 
 	@Id
 	@GeneratedValue(strategy = IDENTITY)
-	@Column(name = "id")
 	private long id;
 
-	@Column(name = "n")
-	private String type;
-	
+	@NotNull
+	@Column(name = "type", nullable = false, columnDefinition = "CHAR(8)")
+	@Enumerated(EnumType.STRING)
+	private ChatMessageType type;
+
+	@NotNull
+	@Column(name = "content", nullable = false)
 	private String content;
 
+	@NotNull
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "room_id", nullable = false)
 	private Room room;
 
+	@NotNull
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
 	private User user;
 }
